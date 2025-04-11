bda6d0ab9b10a59a2b04cff665c2c3b4cf177494
Salman Sadiq <155360332+salmansadiq121@users.noreply.github.com>
2025-04-11 20:30:58 +0500

    Update page.js

--- src/app/(pages)/faqs/page.js
+++ src/app/(pages)/faqs/page.js
@@ -1,3 +1,4 @@
+"use client"
 import Faqs from "@/app/components/Faqs";
 import MainLayout from "@/app/components/Layout/Layout";
 import React from "react";
@@ -6,7 +7,7 @@ export default function FAQPage() {
   return (
     <MainLayout title={"Ayoob - FAQ's"}>
       <div className="w-full min-h-[70vh] py-4 px-4">
-        <Faqs />
+        // <Faqs />
       </div>
     </MainLayout>
   );

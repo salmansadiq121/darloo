35b790a0c730c63799da7f3f2a2113d7a449ae61
Salman Sadiq <salmansadiq1015@gmail.com>
2025-09-12 12:43:42 +0500

    images issue fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["*"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "**.alicdn.com", // supports cbu01.alicdn.com and others
-      },
-      {
-        protocol: "https",
-        hostname: "s3.amazonaws.com", // example AWS
-      },
-      {
-        protocol: "https",
-        hostname: "*.amazonaws.com", // wildcard AWS bucket domains
-      },
-      {
-        protocol: "https",
-        hostname: "*.alibaba.com", // if you also need alibaba images
-      },
-    ],
-    unoptimized: true,
-  },
+  unoptimized: true,
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "**",
+    },
+    {
+      protocol: "https",
+      hostname: "secure.gravatar.com",
+    },
+    {
+      protocol: "https",
+      hostname: "flagcdn.com",
+    },
+    {
+      protocol: "https",
+      hostname: "**.alicdn.com", // supports cbu01.alicdn.com and others
+    },
+    {
+      protocol: "https",
+      hostname: "s3.amazonaws.com", // example AWS
+    },
+    {
+      protocol: "https",
+      hostname: "*.amazonaws.com", // wildcard AWS bucket domains
+    },
+    {
+      protocol: "https",
+      hostname: "*.alibaba.com", // if you also need alibaba images
+    },
+  ],
   reactStrictMode: true,
 };
 

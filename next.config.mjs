a9d2fc3d7818de362ed676a1a3e420efb184f534
Salman Sadiq <salmansadiq1015@gmail.com>
2025-09-14 21:15:15 +0500

    return histroy, return products

--- next.config.mjs
+++ next.config.mjs
@@ -21,10 +21,54 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
+  async rewrites() {
+    return [
+      {
+        source: "/robots.txt",
+        destination: "/api/robots",
+      },
+    ];
   },
+
+  webpack(config, { isServer }) {
+    // Handle SVG with @svgr/webpack
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    config.externals.push({
+      "thread-stream": "commonjs thread-stream",
+    });
+
+    return config;
+  },
+
+  images: {
+    unoptimized: true, // allows all external images without optimization
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // allow all https hosts
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "**", // allow all http hosts too (if needed)
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "secure.gravatar.com",
+      },
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+      },
+    ],
+  },
+
+  staticPageGenerationTimeout: 280,
 };
 
 export default nextConfig;

e8e674702ebc0cdd36623223c5dfd8747b3aed30
Salman Sadiq <salmansadiq1015@gmail.com>
2025-09-14 21:06:23 +0500

    return histroy, return products

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,30 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["*"],
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "cbu01.alicdn.com",
+//       },
+//     ],
+//     unoptimized: true,
+//   },
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    domains: ["*"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "cbu01.alicdn.com",
-      },
-    ],
     unoptimized: true,
   },
-  reactStrictMode: true,
 };
 
 export default nextConfig;

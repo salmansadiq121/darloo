7c89484c7c6051ef3c57ee26fd821fe80681afa5
Salman Sadiq <salmansadiq1015@gmail.com>
2025-09-29 12:41:12 +0500

    error fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/robots.txt",
-        destination: "/api/robots",
-      },
-    ];
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
   images: {
-    unoptimized: true,
+    domains: ["*"],
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "darloo.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "secure.gravatar.com",
-      },
-      {
-        protocol: "https",
-        hostname: "flagcdn.com",
-      },
       {
         protocol: "https",
         hostname: "**",
@@ -77,74 +48,9 @@ const nextConfig = {
         pathname: "**",
       },
     ],
+    unoptimized: true,
   },
-  // experimental: {
-  //   serverComponentsExternalPackages: ["pino", "pino-pretty"],
-  // },
-  webpack: (config, context) => {
-    config.externals.push({
-      "thread-stream": "commonjs thread-stream",
-    });
-    return config;
-  },
-  staticPageGenerationTimeout: 280,
+  reactStrictMode: false,
 };
 
 export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["*"],
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "cbu01.alicdn.com",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "*.alicdn.com",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "lh3.googleusercontent.com",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "avatars.githubusercontent.com",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "s3.eu-north-1.amazonaws.com",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "secure.gravatar.com",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "flagcdn.com",
-//         pathname: "/**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "cdn.shopify.com",
-//         pathname: "**",
-//       },
-//     ],
-//     unoptimized: true,
-//   },
-//   reactStrictMode: false,
-// };
-
-// export default nextConfig;

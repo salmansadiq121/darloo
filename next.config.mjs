7bb0945ba4946f7a00c43fbedc9b221d13e10fcc
Salman Sadiq <salmansadiq1015@gmail.com>
2025-10-06 10:42:06 +0500

    images issue fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,59 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["*"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "**",
       },
       {
-        protocol: "https",
-        hostname: "cbu01.alicdn.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "*.alicdn.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "s3.eu-north-1.amazonaws.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "secure.gravatar.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "flagcdn.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.shopify.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cbu01.alicdn.com",
+        protocol: "http",
+        hostname: "**",
       },
     ],
     unoptimized: true,
   },
-  reactStrictMode: false,
 };
 
 export default nextConfig;

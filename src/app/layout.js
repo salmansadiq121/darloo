742c439ce948f834248cd4444d8a1e20c8291a70
Salman Sadiq <salmansadiq1015@gmail.com>
2025-10-14 15:22:30 +0500

    bot adding

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,35 +38,35 @@ export default function RootLayout({ children }) {
     document.body.appendChild(gafScript);
 
     /** ✅ ChatBot.com script loader (ChatBot widget before </body>) */
-    const chatbotScript = document.createElement("script");
-    chatbotScript.async = true;
-    chatbotScript.innerHTML = `
-      window.__ow = window.__ow || {};
-      window.__ow.organizationId = "9a62d47f-7b24-47fc-8a08-35ab3ae8d980";
-      window.__ow.template_id = "b352d5d4-c9de-4e65-a39c-01338d07e1bc";
-      window.__ow.integration_name = "manual_settings";
-      window.__ow.product_name = "chatbot";
+    // const chatbotScript = document.createElement("script");
+    // chatbotScript.async = true;
+    // chatbotScript.innerHTML = `
+    //   window.__ow = window.__ow || {};
+    //   window.__ow.organizationId = "9a62d47f-7b24-47fc-8a08-35ab3ae8d980";
+    //   window.__ow.template_id = "b352d5d4-c9de-4e65-a39c-01338d07e1bc";
+    //   window.__ow.integration_name = "manual_settings";
+    //   window.__ow.product_name = "chatbot";
 
-      (function(n,t,c){
-        function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}
-        var e={_q:[],_h:null,_v:"2.0",
-          on:function(){i(["on",c.call(arguments)])},
-          once:function(){i(["once",c.call(arguments)])},
-          off:function(){i(["off",c.call(arguments)])},
-          get:function(){if(!e._h)throw new Error("[OpenWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},
-          call:function(){i(["call",c.call(arguments)])},
-          init:function(){
-            var n=t.createElement("script");
-            n.async=!0;
-            n.type="text/javascript";
-            n.src="https://cdn.openwidget.com/openwidget.js";
-            t.head.appendChild(n);
-          }};
-        !n.__ow.asyncInit && e.init();
-        n.OpenWidget = n.OpenWidget || e;
-      }(window,document,[].slice));
-    `;
-    document.body.appendChild(chatbotScript);
+    //   (function(n,t,c){
+    //     function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}
+    //     var e={_q:[],_h:null,_v:"2.0",
+    //       on:function(){i(["on",c.call(arguments)])},
+    //       once:function(){i(["once",c.call(arguments)])},
+    //       off:function(){i(["off",c.call(arguments)])},
+    //       get:function(){if(!e._h)throw new Error("[OpenWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},
+    //       call:function(){i(["call",c.call(arguments)])},
+    //       init:function(){
+    //         var n=t.createElement("script");
+    //         n.async=!0;
+    //         n.type="text/javascript";
+    //         n.src="https://cdn.openwidget.com/openwidget.js";
+    //         t.head.appendChild(n);
+    //       }};
+    //     !n.__ow.asyncInit && e.init();
+    //     n.OpenWidget = n.OpenWidget || e;
+    //   }(window,document,[].slice));
+    // `;
+    // document.body.appendChild(chatbotScript);
   }, []);
 
   return (
@@ -134,7 +134,7 @@ export default function RootLayout({ children }) {
         </SessionProvider>
       </body>
       {/* ✅ Fallback message if JS disabled */}
-      <noscript>
+      {/* <noscript>
         You need to{" "}
         <a
           href="https://www.chatbot.com/help/chat-widget/enable-javascript-in-your-browser/"
@@ -151,7 +151,12 @@ export default function RootLayout({ children }) {
           ChatBot.com
         </a>
         .
-      </noscript>
+      </noscript> */}
+      <script
+        defer
+        src="https://app.fastbots.ai/embed.js"
+        data-bot-id="cmgqdlbij0bdwqm1kuw4swndq"
+      ></script>
     </html>
   );
 }
